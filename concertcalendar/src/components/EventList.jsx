ddb69cec58838138d2550eaaee20b1d26505f937
Brian Weitz <brianweitz@Brians-MacBook-Pro-2.local>
2019-06-30 11:30:04 -0400

    styled search fail message

--- concertcalendar/src/components/EventList.jsx
+++ concertcalendar/src/components/EventList.jsx
@@ -14,16 +14,16 @@ class EventList extends React.Component {
         <main id='list-main'>
           {this.props.eventData ?
             this.props.eventData.map(event => (
-              <a className='event-changer' key={event.id}>
-                <div className="event" onClick={() =>
-                  this.props.saveEvent(event.id)}>
-                  <h3>{event.name}</h3>
-                  <p>Location: {event._embedded.venues[0].name}</p>
-                  <p>Start time: {format(event.dates.start.dateTime, 'h:mm A')}</p>
-                  <p className='add-event'>Add to calendar</p>
-                </div>
-              </a>
-            )) : <h3>No events available in your area on that date, sorry!</h3>}
+              // <a className='event-changer'>
+              <div className="event" key={event.id} onClick={() =>
+                this.props.saveEvent(event.id)}>
+                <h3>{event.name}</h3>
+                <p>Location: {event._embedded.venues[0].name}</p>
+                <p>Start time: {format(event.dates.start.dateTime, 'h:mm A')}</p>
+                <p className='add-event'>Add to calendar</p>
+              </div>
+              // </a>
+            )) : <div id="sorry"><h3>No events available in your area on that date, sorry!</h3></div>}
         </main>
       </div>
     )
